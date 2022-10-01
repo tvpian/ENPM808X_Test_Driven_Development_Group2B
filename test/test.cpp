baf201e220261cfd0138822f3eb1df247620c56a
Tharun Puthanveettil <tharunv.pvit@yahoo.com>
2022-10-01 01:48:23 -0400

    jp remote branch test withupdated testsuites

--- test/test.cpp
+++ test/test.cpp
@@ -10,6 +10,14 @@ TEST(dummy, should_pass) {
 
 
 TEST(TDD_PairB, Testsuite) {
+  double param1, param2, param3;
   pid controller(1.0, 0.0, 0.25);
   controller.calcPID(0.1, 0.5);
+  param1 = controller.getParam("kp");
+  param2 = controller.getParam("ki");
+  param3 = controller.getParam("kd");
+  EXPECT_EQ(param1,1.0);
+  EXPECT_EQ(param2,0.0);
+  EXPECT_EQ(param3,0.25);
+  EXPECT_EQ(controller.getParam("nothing"),0);
 }
