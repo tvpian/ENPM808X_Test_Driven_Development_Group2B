b1af61f5643377b9fbb5e254833535d9e7025e06
Tharun Puthanveettil <tharunv.pvit@yahoo.com>
2022-10-01 00:49:15 -0400

    Added basic test cases forthe pid.hpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,15 @@
+// "Copyright [2022] Tharun V. Puthanveettil"
+
 #include <gtest/gtest.h>
+#include "pid.hpp"
 
 TEST(dummy, should_pass) {
   EXPECT_EQ(1, 1);
+}
+
+
+
+TEST(TDD_PairB, Testsuite) {
+  pid controller(1.0, 0.0, 0.25);
+  controller.calcPID(0.1, 0.5);
 }
